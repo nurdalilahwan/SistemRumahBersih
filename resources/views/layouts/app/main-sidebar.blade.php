fa236c5fd7defad733a1c72a3bd1c1b1091cc0be
nurdalilahwan <nurdalilahwan@gmail.com>
2022-01-11 15:32:55 +0800

    asdasdasddasd

--- resources/views/layouts/app/main-sidebar.blade.php
+++ resources/views/layouts/app/main-sidebar.blade.php
@@ -2,13 +2,13 @@
 <<<<<<< HEAD
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
-    <img src="{{ asset('img/AdminLTE Logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="{{ asset('img/rumah bersih-logos_transparent.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">AdminLTE 3</span>
   </a>
 =======
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <img src="{{ asset('img/AdminLTE Logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset('img/rumah bersih-logos_transparent.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 >>>>>>> 3f3912552a54ab18abed43ab16a49b071bd6fa9c
