8183ff5b39bd1f7d29a8685dd13d60622b965b51
syamilwan <syamil.wan46@gmail.com>
2022-01-12 03:27:54 +0800

    fix

--- resources/views/layouts/app/main-sidebar.blade.php
+++ resources/views/layouts/app/main-sidebar.blade.php
@@ -1,18 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-<<<<<<< HEAD
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
-    <img src="{{ asset('img/rumah bersih-logos_transparent.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <img src="{{ asset('img/rumah bersih-logos_transparent.png') }}" alt="Rumah Bersih" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">Rumah Bersih</span>
   </a>
-=======
-    <!-- Brand Logo -->
-    <a href="#" class="brand-link">
-        <img src="{{ asset('img/rumah bersih-logos_transparent.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
->>>>>>> 3f3912552a54ab18abed43ab16a49b071bd6fa9c
-
     <!-- Sidebar -->
     <div class="sidebar">
 
