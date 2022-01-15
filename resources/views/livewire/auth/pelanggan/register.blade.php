93acd59284233a13ada4d5bb0cde39acbf7c7fa9
syamilwan <syamil.wan46@gmail.com>
2022-01-16 02:38:51 +0800

    fix

--- resources/views/livewire/auth/pelanggan/register.blade.php
+++ resources/views/livewire/auth/pelanggan/register.blade.php
@@ -7,41 +7,43 @@
             <x-slot name="body">
                 <form id="register-form"  wire:submit.prevent="register">
 
-                    <div class="row">
-                        <div class="col-6">
-                            <x-forms.text wire:model="daftarPelanggan.nama_penuh" label="Nama Penuh" required />
-                            <x-forms.text wire:model="daftarPelanggan.email" label="Email"  required />
-                            <x-forms.text wire:model="daftarPelanggan.name" label="Nama Pengguna" required />
-                        </div>
-                        <div class="col-6">
-                            <x-forms.text wire:model="daftarPelanggan.no_pengenalan" label="No. Kad Pengenalan" required />
-                            <x-forms.text wire:model="daftarPelanggan.no_telefon" label="No Telefon Bimbit" required/>
-                            <x-forms.text wire:model="daftarPelanggan.password" label="Kata Laluan" required />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <x-forms.text wire:model="daftarPelanggan.alamat" label="Alamat" required />
-                        </div>
-                    </div>
-
-                    {{-- <x-card title="Alamat Tetap" collapseButton>
+                    <x-card title="Maklumat Pendaftaran" style="width: 90%; margin: 0 auto; float: none; margin-bottom: 10px;">
+                        <x-slot name="body">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <x-forms.text wire:model="daftarPelanggan.nama_penuh" label="Nama Penuh" required />
+                                        <x-forms.text wire:model="daftarPelanggan.email" label="Email"  required />
+                                    </div>
+                                    <div class="col-6">
+                                        <x-forms.text wire:model="daftarPelanggan.no_pengenalan" label="No. Kad Pengenalan" required />
+                                        <x-forms.text wire:model="daftarPelanggan.no_telefon" label="No Telefon Bimbit" required/>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <x-forms.text wire:model="daftarPelanggan.alamat" label="Alamat" required />
+                                    </div>
+                                </div>
+                        </x-slot>
+                    </x-card>
+                    <x-card title="Maklumat Akaun" style="width: 90%; margin: 0 auto; float: none; margin-bottom: 10px;">
                         <x-slot name="body">
                             <div class="row">
-                                <div class="col-md-6">
-                                    <x-forms.text label="Alamat 1" wire:model="daftarPelanggan.alamat_tetap1" />
-                                    <x-forms.text label="Alamat 2" wire:model="daftarPelanggan.alamat_tetap2" />
-                                    <x-forms.text label="Alamat 3" wire:model="daftarPelanggan.alamat_tetap3" />
+                                <div class="col-3">
                                 </div>
-                                <div class="col-md-6">
-                                    <x-forms.text label="Poskod" wire:model="daftarPelanggan.poskod" />
-                                    <x-forms.text wire:model="postcode.bandar.nama" label="Bandar" readonly required />
-                                    <x-forms.text wire:model="postcode.bandar.negeri.nama" label="Negeri" readonly required />
+                                <div class="col-6">
+                                    <x-forms.text wire:model="daftarPelanggan.name" label="Nama Pengguna" required />
+                                    <x-forms.text wire:model="daftarPelanggan.password" label="Kata Laluan" required />
+                                </div>
+                                <div class="col-3">
                                 </div>
                             </div>
                         </x-slot>
-                    </x-card> --}}
-
+                        <x-slot name="footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal" >Close</button>
+                            <button id="form_hantar" type="submit" class="btn btn-primary" form="register-form">Daftar</button>
+                        </x-slot>
+                    </x-card>
                 </form>
             </x-slot>
             <x-slot name="footer">
